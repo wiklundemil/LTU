887f7b20717076205801d2a3895b4d88f28210fd
wiklundemil <wiklund.emil2000@hotmail.com>
2020-09-03 21:44:58 +0200

    Uppdate uppgift 1 och 2
    Fortsatte

--- D0009E/Laboration1/Uppgift1.py
+++ D0009E/Laboration1/Uppgift1.py
@@ -1,19 +1,14 @@
-def cost(loan_amount, years, yearly_interest):
-    cost=(loan_amount+(years+1)*(loan_amount*yearly_interest)/2)
-    return cost
+def cost(loan_amount, years, yearly_interest):                                #Här skapar vi en funktion som erhåller variablerna "loan_amount", "years", "yearly_interest."
+    cost=(loan_amount+(years+1)*(loan_amount*yearly_interest)/2)              #Detta är formeln för att räkna ut kostnaden.
+    return cost                                                               #return ger oss möjligheten att använda variabeln cost vidare. 
 
 
-loan_amount = int(input("Sriv in det lånade beloppet:"))
-years = int(input("skriv in antal år för återbetalning:"))
-yearly_interest = float(input("Skriv in den årliga räntesatsen:"))
+loan_amount = int(input("Sriv in det lånade beloppet:"))                      #Frågar om vad det lånade beloppet är. Via input() sätter man variabeln.
+years = int(input("skriv in antal år för återbetalning:"))                    #Samma som ovan.
+yearly_interest = float(input("Skriv in den årliga räntesatsen:"))            #Samma som ovan.
 
 
-#loan_amount = 50000
-#years = 10
-#yearly_interest = 0.03
+result = cost(loan_amount, years, yearly_interest)                            #Skapar variabeln "result" för att kunna spara ned värdet man får när man kallar cost-funktionen.
+print("Den totala kostnaden efter",years,"år är "+ str(result) +"kr.")        #Skriver ut hur många år i texten samt omvandlar "result" värdet till en string. 
 
-
-result = cost(loan_amount, years, yearly_interest)
-print("Den totala kostnaden efter",years,"år är "+ str(result) +"kr.")
-
-input("Klicka enter för att avsluta.")
+input("Klicka enter för att avsluta.")                                        #Om man kör detta i shell så kommer programmet avslutas. Därav input() för att stanna upp programmet.
