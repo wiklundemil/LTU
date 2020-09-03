e622cf3d3ab5a65aee09eab42844c1862e42beeb
wiklundemil <wiklund.emil2000@hotmail.com>
2020-09-03 16:00:12 +0200

    Uppladning från laåtop
    Filer från laptop

---
+++ Python projetcts/Laboration1/Uppgift2.py
@@ -0,0 +1,11 @@
+def recept_sockerkaka(antal):
+    print("Detta recept är för",antal,"Personer")
+    lista_recept_sockerkaka = ["Ägg","Strösocker","Vanlijsocker","Bakpulver","Vetemjöl","Smör","Vatten"]
+    
+    print(lista_recept_sockerkaka)
+    lista_recept_sockerkaka
+    
+asdsd
+recept_sockerkaka(1)
+
+sdsd
