18b3fea9e70011ddf96f53d9df48a7e98033a270
wiklundemil <wiklund.emil2000@hotmail.com>
2020-09-17 12:18:12 +0200

    Lade filer i rätt mappar

--- Learning Python/MathFunctions.py
+++
@@ -1,9 +0,0 @@
-import math
-
-print(math.ceil(2.9)) #Google pythonn 3 math module for explaniation for every module
-print(math.floor(2.9)) #Google pythonn 3 math module for explaniation for every module
-x = 2.9 
-print(round(x)) #Avrundar 
-print(abs(-2.9)) #Gör talet alltid till positivt
-
-
