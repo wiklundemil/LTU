e697475205ecb7a86439fba6e2302508f783aaf6
wiklundemil <wiklund.emil2000@hotmail.com>
2020-09-25 10:54:27 +0200

    update

--- D0015E/ModulF/js/alert.js
+++ D0015E/ModulF/js/alert.js
@@ -1,3 +1,5 @@
+// Script fil som printar ut nedanstående som en
+//varning när du försöker navigera ifrån spelet på sidan.
 function alertFunction() {
   alert("Detta kommer starta om ditt spel!");
 }
